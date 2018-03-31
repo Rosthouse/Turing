ddd53a38cc53621163e7b9730911b877c99ac400
Patrick Joos <patrick.joos@ntb.ch>
2018-03-31 11:15:36 +0200

    CHANGE: Setting up the project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Turing Machine
+
+In the course "Theoretical Computation", we discussed the turing machine. I wanted to see how fast I could implement one.
--- src/TuringMachine.ts
+++ src/TuringMachine.ts
@@ -1,8 +1,11 @@
+import * as fs from "fs-extra";
+
 import { Configuration } from "./Configuration";
 
 export class TuringMachine {
 
     readConfiguration(path: string): Configuration {
         JSON.parse(fs.readFileSync(path));
+        return undefined;
     }
 }
